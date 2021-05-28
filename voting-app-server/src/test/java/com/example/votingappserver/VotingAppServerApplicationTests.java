8b44fc15db810d1a20a84cc38d774434e3c93ee3
Jero Peter <j.peter@micromata.de>
2021-05-28 20:45:22 +0200

    create multi-module project:voting-app

---
+++ voting-app-server/src/test/java/com/example/votingappserver/VotingAppServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.votingappserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotingAppServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
